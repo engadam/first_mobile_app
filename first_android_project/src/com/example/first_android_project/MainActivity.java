50933263a78f1ad172c2b50471e552ba66145fd2
adam <adamsworld2005@hotmail.com>
2015-05-07 10:49:21 +0200

    btn one error finished

--- first_android_project/src/com/example/first_android_project/MainActivity.java
+++ first_android_project/src/com/example/first_android_project/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 	}
 
 	private void shownumber() {
-		
+		//----------------------------------------------------------
 		 Button b0=(Button) findViewById(R.id.btn_0);
 		 
 		 b0.setOnClickListener(new View.OnClickListener() {
@@ -33,8 +33,57 @@ public class MainActivity extends Activity {
 			}
 		});
 		
-		
-		
+		//----------------------------------------------------------	
+
+		 Button b1=(Button) findViewById(R.id.btn_1);
+		 
+		 b1.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView tv=(TextView)findViewById(R.id.num_disp);
+				String one="1";
+				String s1=(String)tv.getText();
+				tv.setText(s1+one);
+				
+			}
+		});
+
+		//----------------------------------------------------------	
+
+		 Button b2=(Button) findViewById(R.id.btn_2);
+		 
+		 b2.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView tv=(TextView)findViewById(R.id.num_disp);
+				String two="2";
+				String s1=(String)tv.getText();
+				tv.setText(s1+two);
+				
+			}
+		});
+		 
+		 
+		//----------------------------------------------------------	
+
+		 Button b3=(Button) findViewById(R.id.btn_3);
+		 
+		 b3.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView tv=(TextView)findViewById(R.id.num_disp);
+				String three="3";
+				String s1=(String)tv.getText();
+				tv.setText(s1+three);
+				
+			}
+		});
+
+		 
+		 
 	 Button b7=(Button) findViewById(R.id.btn_7);
 	 
 	 b7.setOnClickListener(new View.OnClickListener() {
