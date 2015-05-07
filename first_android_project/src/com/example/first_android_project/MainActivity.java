81eaf24af5e9151433668fa3a41dabbf7e02d989
adam <adamsworld2005@hotmail.com>
2015-05-07 02:43:23 +0200

    button Zero, One and Seven are working

--- first_android_project/src/com/example/first_android_project/MainActivity.java
+++ first_android_project/src/com/example/first_android_project/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.first_android_project;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +13,44 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		shownumber();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void shownumber() {
+		
+		 Button b0=(Button) findViewById(R.id.btn_0);
+		 
+		 b0.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				TextView tv=(TextView)findViewById(R.id.num_disp);
+				String zero="0";
+				String s1=(String)tv.getText();
+				tv.setText(s1+zero);
+				
+			}
+		});
+		
+		
+		
+	 Button b7=(Button) findViewById(R.id.btn_7);
+	 
+	 b7.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			TextView tv=(TextView)findViewById(R.id.num_disp);
+			String sevn="7";
+			String s1=(String)tv.getText();
+			tv.setText(s1+sevn);
+			
+		}
+	});
+	 
 	}
+
+	
 
 }
